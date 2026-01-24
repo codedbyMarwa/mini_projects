7aac6813ff95bfe6c8ff2799e1f7c42eab3623fe
codedbyMarwa <marwaqasim.cs@gmail.com>
2026-01-24 21:49:36 +0500

    added student profile generator

---
+++ studentProfile.py
@@ -0,0 +1,15 @@
+name=input("Enter your name: ")
+rollno=input("Enter your roll no: ")
+age=int(input("Enter your age: "))
+cgpa=float(input("Enter your CGPA: "))
+
+print(f"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Name: {name}
+Roll no: {rollno}
+Age: {age}
+CGPA: {cgpa}
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Profile Successfully created.
+
+""")
